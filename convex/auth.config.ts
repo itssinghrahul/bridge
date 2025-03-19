3976fe9e6852899c88a383115e3c984fb28c2c1b
itssinghrahul <rahulsingh.prs@gmail.com>
2025-03-19 23:12:17 +0530

    For adding the initial files

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://ample-sunbird-46.clerk.accounts.dev',
+      applicationID: 'convex'
+    }
+  ]
+}
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['robust-gerbil-129.convex.cloud']
+  }
+}
+
+export default nextConfig
